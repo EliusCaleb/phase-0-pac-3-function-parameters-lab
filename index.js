0421aded0da6fd04608390e4cfba1362681c3312
ELIUS <eliuswanyoro@gmail.com>
2022-09-05 14:57:28 +0300

    Done

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-function introduction(name) {
-    return(`Hi, my name is ${name}.`);  
+function  introduction(name){
+    return `Hi, my name is ${name}.`
 }
-   console.log (introduction("Aki"));
+introduction('Aki');
 
-function introductionWithLanguageOptional(name, language){
-    return( `Hi, my name is ${name} and I am learning to program in ${language}.`);
+function   introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguage('Samip', 'React');
 
+function  introductionWithLanguageOptional(name, language='JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
- 
- console.log(introductionWithLanguageOptional("Gracie", "Python" ));
-
-function introductionWithLanguage(name, language) {
-    return( `Hi, my name is ${name} and I am learning to program in ${language}.`);
-}
- 
-function introductionWithLanguageOptional(name,language="JavaScript"){
-    return( `Hi, my name is ${name} and I am learning to program in ${language}.`)
-}
-console.log(introductionWithLanguageOptional("Gracie", "Python" ))
+introductionWithLanguage('Gracie');
+introductionWithLanguageOptional('Gracie', 'Python')
